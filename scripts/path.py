a1dba240df3e1f7da66dfaa0aff0eeb18d4d4ade
Ryan Stenmark <rstenmark>
2022-08-22 22:12:10 -0600

    Added paths.py

---
+++ scripts/path.py
@@ -0,0 +1,15 @@
+import pathlib
+
+# This is the absolute path to the root directory of the project.
+# Replace the path string with the correct path if it differs from
+# the one provided by default.
+root: pathlib.Path = pathlib.Path("/home/rs/prosper2")
+
+# Absolute path to the scripts directory (folder)
+scripts: pathlib.Path = pathlib.Path(root / "scripts")
+
+# Absolute path to the sqlite3 database (file)
+database: pathlib.Path = pathlib.Path(root / "database/database.db")
+
+# Absolute path to the logging directory (folder)
+logging: pathlib.Path = pathlib.Path(root / "logging")
