c53296eab73759bf61df7877030c7d0a1bdcad56
Ryan Stenmark <rstenmark>
2022-09-02 22:17:37 -0600

    Update create_database.py

--- create_database.py
+++ create_database.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3.11
 import sqlite3
-import prosper2.path
-import prosper2.database
+import prosper2.path as path
+import prosper2.database.query as query
 
 # Connect to the database, creating the file
 # if it does not already exist:
-with sqlite3.connect(prosper2.path.database) as con:
+with sqlite3.connect(path.database) as con:
     # Get a cursor:
     cur = con.cursor()
     # Create tables
-    prosper2.database.create_table_exchange_derived(cur)
-    prosper2.database.create_table_exchange_orders(cur)
-    prosper2.database.create_table_material_all(cur)
-    prosper2.database.create_table_exchange_station(cur)
+    query.create_table_exchange_derived(cur)
+    query.create_table_exchange_orders(cur)
+    query.create_table_material_all(cur)
+    query.create_table_exchange_station(cur)
     # Commit changes
     con.commit()
 
